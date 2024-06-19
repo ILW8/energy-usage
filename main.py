9389ead6a7e5c93115caf08ed1e1766fe04d1586
Dao Heng Liu <daohengliu@gmail.com>
2024-06-19 01:14:36 +0100

    insert parsed usage data into intervaltree

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+import csv
+import datetime
+from typing import Union
+from tqdm import tqdm
 import pandas as pd
 from pathlib import Path
 import requests
 import sys
+from intervaltree import IntervalTree
+# from matplotlib import pyplot
 
+
+XLSX_PATH = 'Eve_Energy_1908_Total_Consumption.xlsx'
+CSV_PATH = 'Eve_Energy_1908_Total_Consumption.csv'
 
 BASE_URL = "https://api.octopus.energy"
 PRODUCT_CODE = "AGILE-18-02-21"
@@ -10,32 +19,33 @@ TARIFF_CODE = f"E-1R-{PRODUCT_CODE}-C"
 TARIFF_URL = f"{BASE_URL}/v1/products/{PRODUCT_CODE}/electricity-tariffs/{TARIFF_CODE}"
 UNIT_RATE_URL = f"{TARIFF_URL}/standard-unit-rates/"
 
+# can't be bothered to do input handing
+START_TIME_UNIX = 1704067200.0  # 1704067200 -- 2024-01-01T00:00:00Z
+END_TIME_UNIX = 1717196400.0  # 1717196400 -- 2024-06-01T00:00:00Z
+
 
 def convert_xlsx_to_csv():
     """
     data from energy reader is in .xlsx and I don't want to deal with pandas DFs
     :return: void
     """
-    xlsx_path = 'Eve_Energy_1908_Total_Consumption.xlsx'
-    csv_path = 'Eve_Energy_1908_Total_Consumption.csv'
-
-    if not Path(xlsx_path).exists():
-        print(f"missing input file '{xlsx_path}'")
+    if not Path(XLSX_PATH).exists():
+        print(f"missing input file '{XLSX_PATH}'")
         sys.exit(1)
 
     needs_updating = True
-    if Path(csv_path).exists():
-        input_mtime = Path(xlsx_path).stat().st_mtime
-        csv_mtime = Path(csv_path).stat().st_mtime
+    if Path(CSV_PATH).exists():
+        input_mtime = Path(XLSX_PATH).stat().st_mtime
+        csv_mtime = Path(CSV_PATH).stat().st_mtime
         if csv_mtime > input_mtime:
             print(f"output is newer than input, skipping conversion ({csv_mtime} > {input_mtime})")
             needs_updating = False
 
-    if needs_updating:
-        df = pd.read_excel(xlsx_path)
+    if needs_updating or True:
+        df = pd.read_excel(XLSX_PATH, header=3)
 
         # truncate misc header data
-        df[2:].to_csv(csv_path, index=False, header=False)
+        df.sort_values(by=["Date"]).to_csv(CSV_PATH, index=False, header=False)
 
 
 def fetch_agile_prices(period_from=None, period_to=None) -> list:
@@ -77,6 +87,33 @@ def fetch_agile_prices(period_from=None, period_to=None) -> list:
 
 if __name__ == '__main__':
     convert_xlsx_to_csv()
-    prices: list = fetch_agile_prices(period_from="2024-01-01T00:00:00Z")
-    prices_sorted: list = sorted(prices, key=lambda entry: entry['valid_from'])
-    print()
+    with open(CSV_PATH, "r") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)  # skip header
+
+        tree = IntervalTree()
+
+        # we want to get a wattage for a time range
+        last_dt: Union[datetime, None] = None
+        for line in tqdm(csv_reader):
+            dt = datetime.datetime.strptime(line[0], "%Y-%m-%d %H:%M:%S")
+
+            if last_dt is not None:
+                time_elapsed: datetime.timedelta = dt - last_dt
+                energy_consumed = float(line[1])
+                wattage = energy_consumed * 3600 / time_elapsed.total_seconds()
+
+                interval_begin = int(dt.timestamp())
+                interval_end = interval_begin + time_elapsed.total_seconds()
+                if START_TIME_UNIX <= interval_end and interval_begin <= END_TIME_UNIX:
+                    tree.addi(interval_begin, interval_end, wattage)
+
+            last_dt = dt
+
+    # pyplot.step([item.begin for item in sorted(tree.items(), key=lambda x: x.begin)],
+    #             [item.data for item in sorted(tree.items(), key=lambda x: x.begin)])
+    # pyplot.show()
+
+    prices_sorted: list = sorted(fetch_agile_prices(period_from="2024-01-01T00:00:00Z"),
+                                 key=lambda entry: entry['valid_from'])
+    # print()
